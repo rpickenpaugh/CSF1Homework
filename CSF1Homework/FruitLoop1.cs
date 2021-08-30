9524998a603a8a37bee0e76cdc0da151a973a6bb
Robert Pickenpaugh <robert.pickenpaugh@outlook.com>
2021-08-30 10:47:39 -0500

    FruitLoop1 commit

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            //Declaring fruits array
+            string[] fruits = new string[6];
+            fruits[0] = "apple";
+            fruits[1] = "orange";
+            fruits[2] = "banana";
+            fruits[3] = "pomegranate";
+            fruits[4] = "papaya";
+            fruits[5] = "pear";
+
+            //Looping the fruits array
+            for (int fruitPosition = 0; fruitPosition < fruits.Length; fruitPosition++)
+            {
+                Console.WriteLine(fruits[fruitPosition]);
+            }
+        }
+    }
+}
